7940a64290693a9eb7b58ef6fdf9fe03e7790785
Delfio <delfio_eu@hotmail.com>
2019-09-24 01:16:52 -0400

    Update register meetapp
    Nova regra para não registrar eventos com datas passadas

--- src/app/controllers/MeetappController.js
+++ src/app/controllers/MeetappController.js
@@ -1,4 +1,5 @@
 import Meetups from '../models/Meetapp';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import * as Yup from 'yup';
 
@@ -18,14 +19,34 @@ class MeetappController {
     }
 
     const {name, descricao, localizacao, date, banner_id} = req.body;
-    console.log(userId);
-    // Checar se o user_id existe
+
+
+    // Checar se o user_id existe tem nem sentido nesse caso, mas só por garantia mesmo
     const isUser = await User.findOne({
       where:{ id: req.userId }
     });
 
     if(!(isUser)){
       return res.status(401).json({error: 'Sessão expirada logue novamente'})
+    }
+    
+    //Converter a data e armazenar em uma variavel
+    //Zerando valores de minutos e segundos
+    const hourStart = startOfHour(parseISO(date));
+
+    if(isBefore(hourStart, new Date())){
+      return res.status(400).json({error: "Não é permitido cadastrar eventos para datas passadas"})
+    }
+
+    //Verificar se o horário já está ocupado
+    const checkAvailability = await Meetups.findOne({
+      where: {
+        date: hourStart,
+        canceled_at: null,
+      }
+    });
+    if(checkAvailability){
+      return res.status(400).json({error: "O horário já está reservado!"})
     }
 
     const meetappExists = await Meetups.findOne({ where: {name: req.body.name} });
